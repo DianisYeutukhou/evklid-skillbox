9bffd89b5e7ec0c23c8dd7e17653497cde7bdf8a
Denis Yeutukhou <yeutukhoudianis@mail.ru>
2022-05-10 23:30:26 +0300

    tabs ready

--- js/script.js
+++ js/script.js
@@ -17,3 +17,18 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+let tabsBtn = document.querySelectorAll('.work__btn');
+let tabsItem = document.querySelectorAll('.work__info');
+
+tabsBtn.forEach(function(element){
+  element.addEventListener('click', function(e){
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function(btn){ btn.classList.remove('work__btn-active')});
+    e.currentTarget.classList.add('work__btn-active');
+
+    tabsItem.forEach(function(element){ element.classList.remove('work__info-active')});
+    document.querySelector(`[data-target="${path}"]`).classList.add('work__info-active');
+  });
+});
